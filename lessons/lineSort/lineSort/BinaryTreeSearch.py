f3f00a65b2a538635f40b9005f1c12c3bf2fd986
Alexander-ASU <www-sanguruinbox-ru2014@yandex.ru>
2017-09-16 20:51:28 +0300

    add code

--- lessons/lineSort/lineSort/BinaryTreeSearch.py
+++ lessons/lineSort/lineSort/BinaryTreeSearch.py
@@ -1,20 +1,19 @@
-﻿from binarytree import Node
+﻿from binarytree import Node, BinaryTree
 #import hashlib
 class NodeWithData(Node):
-    def __init__(self, firstWord, secondWord, fileName):
-        concatination = firstWord + secondWord
-        key = hash(concatination)
+    def __init__(self, firstWord, secondWord):
+        key = NodeWithData.makeKey(firstWord, secondWord)
         super().__init__(key)
         self.firstWord = firstWord
         self.secondWord = secondWord 
+        concatination = firstWord + secondWord
         self.collDict = {concatination: {}}
     def UpdateDict(self, firstWord, secondWord, thirdWord):
         concatination = firstWord + secondWord
         trigs = self.collDict.get(concatination, {})
         count = trigs.get(thirdWord, 0)
         trigs[thirdWord] = count + 1
-        self.collDict[concatination] = trigs
-         
+        self.collDict[concatination] = trigs         
     @staticmethod 
     def makeKey(firstWord, secondWord):
         concatination = firstWord + secondWord
@@ -22,15 +21,13 @@ class NodeWithData(Node):
    
 class BinaryTreeSearch:
     def __init__(self, fileName):
+         self.tree = BinaryTree()
          with open(fileName) as f:
-            buffer = ''
+            flag = True
             for line in f:
-                linewords = re.split(r'\W+|\d+|_+', line)
-                for i in range(linewords):
-                    node = twoWords[i] + twoWords[i + 1]
-                    if self.firstWord[:-1]: 
-                        buffer = self.firstWord
-                        break
+                words = re.split(r'\W+|\d+|_+', line)                               
+                for i in range(len(words)):
+                   self.tree.add(NodeWithData(words[i], words[i+1]))
 if __name__ == '__main__':
     test = NodeWithData('ONE','TWO')
     print(test)
