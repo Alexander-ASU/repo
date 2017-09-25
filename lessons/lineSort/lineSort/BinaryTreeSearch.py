b8a27d5fbd2d0cc01057922a8a50c371eadba4fd
Alexander-ASU <www-sanguruinbox-ru2014@yandex.ru>
2017-09-25 18:15:37 +0300

    start

--- lessons/lineSort/lineSort/BinaryTreeSearch.py
+++ lessons/lineSort/lineSort/BinaryTreeSearch.py
@@ -28,10 +28,22 @@ class BinaryTreeSearch:
          with open(fileName) as f:
             for line in f:
                 words = re.split(r'\W+|\d+|_+', line)                               
-                for i in range(len(words)):
-                    self.tree.add(NodeWithData(words[i], words[i+1]))
+                for i in range(len(words) - 2):
+                    w1,w2 = words[i].lower(),words[i].lower()
+                    node = self.tree.find(NodeWithData.makeKey(w1, w2))
+                    if node == None:
+                        node = NodeWithData(w1, w2)
+                        self.tree.add(node)
+                    node.UpdateDict(w1, w2, words[i+2].lower())
+    def search(self, first, second):
+         first, second = first.lower(), second.lower()
+         trig = []
+         self.tree.find()
+         
 
 if __name__ == '__main__':
-    test = BinaryTreeSearch('text.txt') 
+    #test = BinaryTreeSearch('text.txt')
+     
+    test1 = BinaryTreeSearch('text.txt')
     #test = NodeWithData('ONE','TWO')
-    print(test);
+    print(test1)
