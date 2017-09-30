2ae768cf587b313d45a13f47959ceba99f0b9fc7
Alexander-ASU <www-sanguruinbox-ru2014@yandex.ru>
2017-09-30 22:16:08 +0300

    fixes some bugs in BinaryTreeSearch

--- lessons/lineSort/lineSort/BinaryTreeSearch.py
+++ lessons/lineSort/lineSort/BinaryTreeSearch.py
@@ -4,9 +4,7 @@ from binarytree import Node, BinaryTree
 class NodeWithData(Node):
     def __init__(self, firstWord, secondWord):
         key = NodeWithData.makeKey(firstWord, secondWord)
-        super().__init__(key)
-        self.firstWord = firstWord
-        self.secondWord = secondWord 
+        super().__init__(key) 
         concatination = firstWord + secondWord
         self.collDict = {concatination: {}}
     def UpdateDict(self, firstWord, secondWord, thirdWord):
@@ -15,6 +13,11 @@ class NodeWithData(Node):
         count = trigs.get(thirdWord, 0)
         trigs[thirdWord] = count + 1
         self.collDict[concatination] = trigs   
+    def GetTrigrams(self, firstWord, secondWord):
+        concatination = firstWord + secondWord
+        if concatination in self.collDict:
+            return list(self.collDict[concatination].keys())            
+        return [] 
     def __str__(self):
         return str(self.key) + ' ' + str(self.collDict)   
     @staticmethod 
@@ -27,23 +30,28 @@ class BinaryTreeSearch:
          self.tree = BinaryTree()
          with open(fileName) as f:
             for line in f:
-                words = re.split(r'\W+|\d+|_+', line)                               
+                words = re.split(r'\W+|\d+|_+', line)
+                words = list(filter(bool, words))                               
                 for i in range(len(words) - 2):
-                    w1,w2 = words[i].lower(),words[i].lower()
+                    
+                    w1,w2 = words[i].lower(),words[i + 1].lower()
                     node = self.tree.find(NodeWithData.makeKey(w1, w2))
                     if node == None:
                         node = NodeWithData(w1, w2)
                         self.tree.add(node)
                     node.UpdateDict(w1, w2, words[i+2].lower())
+    
     def search(self, first, second):
-         first, second = first.lower(), second.lower()
-         trig = []
-         self.tree.find()
-         
+        first, second = first.lower(), second.lower()
+        key = NodeWithData.makeKey(first, second)
+        node = self.tree.find(key)
+        if node == None: return []
+        return node.GetTrigrams(first, second)  
 
 if __name__ == '__main__':
     #test = BinaryTreeSearch('text.txt')
      
     test1 = BinaryTreeSearch('text.txt')
+    test2 = test1.search('A', 'B')
     #test = NodeWithData('ONE','TWO')
-    print(test1)
+    print(test2)
