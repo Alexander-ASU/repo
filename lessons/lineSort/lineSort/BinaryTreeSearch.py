2ac7e2e85a7c45d20b7eb98b1ca93541a942071f
Alexander-ASU <www-sanguruinbox-ru2014@yandex.ru>
2017-09-30 23:52:31 +0300

    add feature in class BinaryTreeSearch

--- lessons/lineSort/lineSort/BinaryTreeSearch.py
+++ lessons/lineSort/lineSort/BinaryTreeSearch.py
@@ -29,17 +29,30 @@ class BinaryTreeSearch:
     def __init__(self, fileName):
          self.tree = BinaryTree()
          with open(fileName) as f:
+            flag = 0 
             for line in f:
                 words = re.split(r'\W+|\d+|_+', line)
-                words = list(filter(bool, words))                               
+                words = list(filter(bool, words))
+                #if flag != 0:                              
                 for i in range(len(words) - 2):
-                    
-                    w1,w2 = words[i].lower(),words[i + 1].lower()
-                    node = self.tree.find(NodeWithData.makeKey(w1, w2))
-                    if node == None:
-                        node = NodeWithData(w1, w2)
-                        self.tree.add(node)
-                    node.UpdateDict(w1, w2, words[i+2].lower())
+                    if flag == 0 and i == len(words) - 3:
+                        ferstWordOneLine = words[i + 1].lower()
+                        secondWordOneLine = words[i + 2].lower()
+                        node = self.tree.find(NodeWithData.makeKey(ferstWordOneLine, secondWordOneLine))
+                       # node.UpdateDict(ferstWordOneLine, secondWordOneLine, words[i+2].lower())
+                        flag = flag + 1 
+                        break
+                    if i == len(words) - 3:
+                        oneWord = words[i + 1].lower()
+                        twoWord = words[i + 2].lower()
+                        node = self.tree.find(NodeWithData.makeKey(oneWord, twoWord))
+                    else:
+                        w1,w2 = words[i].lower(),words[i + 1].lower()
+                        node = self.tree.find(NodeWithData.makeKey(w1, w2))
+                        if node == None:
+                            node = NodeWithData(w1, w2)
+                            self.tree.add(node)
+                        node.UpdateDict(w1, w2, words[i+2].lower())
     
     def search(self, first, second):
         first, second = first.lower(), second.lower()
